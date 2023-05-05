5d4c0a0f1b427cdcc33908bedd089809fd9cc58e
Ubuntu <ubuntu@ip-172-31-31-153.ec2.internal>
2023-05-05 13:22:30 +0000

    shell script for java app deploing a container

---
+++ bash.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+sudo apt update -y
+sudo apt install openjdk-11-jre -y
+sudo apt install maven -y
+sudo apt update -y
+sudo apt install docker.io -y
+sudo systemctl start docker
+sudo systemctl enable docker
+git clone https://github.com/gangadhariy/JAVAapp.git
+cd JAVAapp
+mvn install
+mvn package
+sudo docker build -t maven .
+sudo docker tag maven gangadhariy/jenkinauto:v1
+sudo docker login -u gangadhariy -p gang@1112
+sudo docker push gangadhariy/jenkinauto:v1
+sudo docker run -itd --name=kelvin -p 80:8080 gangadhariy/jenkinauto:v1 bash
